d1362e5349b93a373af7092e29dd9cc74f458ed1
yans7n <91822716+0x7anshan@users.noreply.github.com>
2026-03-22 21:01:08 +0800

    Update author bio and keywords in blog config

--- blog.config.js
+++ blog.config.js
@@ -16,9 +16,9 @@ const BLOG = {
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'yansyn', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || 'offensive security leaner', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://blog.0xyans7n.top', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '博客,blog,cybersecurity', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '博客,blog,cybersecurity,安全,网安', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
   // BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '', // 备案号 闽ICP备XXXXXX
   // BEI_AN_LINK: process.env.NEXT_PUBLIC_BEI_AN_LINK || 'https://beian.miit.gov.cn/', // 备案查询链接，如果用了萌备等备案请在这里填写
